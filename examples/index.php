e20c362aef9c80098f082650f675df6e4ae64880
Plamen Popov <tzappa@gmail.com>
2014-02-19 16:54:13 +0200

    initial commit

---
+++ examples/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use SugiPHP\Captcha\Captcha;
+
+include __DIR__."/../vendor/autoload.php";
+
+session_start();
+if (isset($_POST["captcha"])) {
+	if (empty($_SESSION["captcha"])) {
+		$msg = "Enter code";
+	} elseif ($_SESSION["captcha"] !== $_POST["captcha"]) {
+		$msg = "Wrong code";
+	} else {
+		unset($_SESSION["captcha"]);
+		die("<h1>Code accepted</h1>");
+	}
+} else {
+	$msg = "";
+}
+
+
+$config = array("charset" => "абвгдежзийклмнопрстуфхцчшщъьюя1234567890");
+$config = array();
+$captcha = new Captcha($config);
+$captcha->build();
+$_SESSION["captcha"] = $captcha->getCode();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<h1><?=$msg;?></h1>
+	<img src="<?=$captcha->base64();?>" />
+	<form method="post" action="">
+		<input type="text" name="captcha" value="<?=$captcha->getCode();?>" />
+		<input type="submit" value="Submit" />
+	</form>
+</body>
+</html>
